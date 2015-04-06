4f7ce6be7dc3113ef15895749ed77e39b945228b
Yan Zhao <zhaoyan1117@gmail.com>
2015-04-06 15:41:17 -0700

    added main

--- ela/__main__.py
+++ ela/__main__.py
@@ -1,18 +1,23 @@
 from __future__ import absolute_import
 
-from ela import ELA
-from ela.metrics import BasicMetricsELA
+from .ela import ELA
+from .metrics import BasicMetricsELA
+from .util import iterate_with_progress
+
+import numpy as np
+import scipy.io as sio
 
 import sys, os
+
 
 def main():
     directory = sys.argv[1]
 
-    for filename in os.listdir(directory):
+    for filename in iterate_with_progress(os.listdir(directory)):
         ela = ELA(directory + '/' + filename)
-        ela_metrics = BasicMetricsELA(ela)
-        print filename
-        print ela_metrics.metrics
+        metrics = BasicMetricsELA(ela)
+        raw_basic_metrics = np.array(metrics.metrics)
+        aggregate_metrics = np.array([metrics.aggregate_mean, metrics.aggregate_variance])
 
 if __name__ == '__main__':
     main()
