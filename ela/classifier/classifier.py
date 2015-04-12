abc3d2a8f2419cf3ae52aa11975aa4100c137941
Yan Zhao <zhaoyan1117@gmail.com>
2015-04-11 21:52:55 -0700

    update with predict message

--- ela/classifier/classifier.py
+++ ela/classifier/classifier.py
@@ -4,25 +4,49 @@ import os.path as op
 
 from sklearn.externals import joblib
 
-from ..ela import ELA
 from ..metrics import BasicMetricsELA
 
 class ELAClassifier(object):
     SERIALIZED_FILE = op.join(op.dirname(op.realpath(__file__)),
                               'ela_classifier/ela_classifier.pkl')
 
+    P1D0 = -0.00225
+    P2D0 = 0.01015
+    N1D0 = -0.02704
+    N2D0 = -0.03943
+    M0   = -0.01464
+    P1D1 = 0.01264
+    P2D1 = 0.0195
+    N1D1 = -0.00107
+    N2D1 = -0.00793
+    M1   = 0.00578
+
+    SURE_AUTH_MSG = "The image is surely authentic."
+    NOT_SURE_AUTH_MSG = "The image may be authentic."
+    NOT_SURE_FAKE_MSG = "The image may be manipulated."
+    SURE_FAKE_MSG = "The image is surely manipulated."
+
     def __init__(self):
         self.classifier = joblib.load(ELAClassifier.SERIALIZED_FILE)
 
-    def predict_with_image_path(self, image_path, raw_output=False):
-        ela = ELA(image_path)
-        return self.predict(ela, raw_output)
-
-    def predict(self, ela, raw_output=False):
+    def predict(self, ela):
         feature = self._build_feature(ela)
-        result = bool(self.classifier.predict(feature)[0])
-        return result if raw_output else \
-            'Manipulated' if result else 'Authentic'
+        return bool(self.classifier.predict(feature)[0])
+
+    def decision_function(self, ela):
+        feature = self._build_feature(ela)
+        return self.classifier.decision_function(feature)[0]
+
+    def predict_message(self, ela):
+        score = self.decision_function(ela)
+        if score < ELAClassifier.N2D1:
+            return ELAClassifier.SURE_AUTH_MSG
+        elif score < 0.0:
+            return ELAClassifier.NOT_SURE_AUTH_MSG
+        elif score < ELAClassifier.P1D1:
+            return ELAClassifier.NOT_SURE_FAKE_MSG
+        else:
+            return ELAClassifier.SURE_FAKE_MSG
 
     def _build_feature(self, ela):
         metrics = BasicMetricsELA(ela)
